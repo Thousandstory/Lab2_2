9ca2ec711c6370d45ac10f7aa5630177213bbb66
totoqqwe <anton.stepanchenko14@gmail.com>
2023-09-17 17:07:37 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp3/Converter.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Converter
+    {
+        private double usdRate;
+        private double eurRate;
+        private double plnRate;
+
+        public Converter(double usd, double eur, double pln)
+        {
+            usdRate = usd;
+            eurRate = eur;
+            plnRate = pln;
+        }
+
+        public double ConvertToUSD(double uah)
+        {
+            return uah / usdRate;
+        }
+
+        public double ConvertToEUR(double uah)
+        {
+            return uah / eurRate;
+        }
+
+        public double ConvertToPLN(double uah)
+        {
+            return uah / plnRate;
+        }
+
+        public double ConvertFromUSD(double usd)
+        {
+            return usd * usdRate;
+        }
+
+        public double ConvertFromEUR(double eur)
+        {
+            return eur * eurRate;
+        }
+
+        public double ConvertFromPLN(double pln)
+        {
+            return pln * plnRate;
+        }
+    }
+}
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Converter converter = new Converter(usd: 38.50, eur: 39.20, pln: 8.20);
+
+            double uahAmount = 1000.0;
+            Console.WriteLine($"{uahAmount} грн = {converter.ConvertToUSD(uahAmount):0.00} USD");
+            Console.WriteLine($"{uahAmount} грн = {converter.ConvertToEUR(uahAmount):0.00} EUR");
+            Console.WriteLine($"{uahAmount} грн = {converter.ConvertToPLN(uahAmount):0.00} PLN");
+
+            double usdAmount = 50.0;
+            double eurAmount = 40.0;
+            double plnAmount = 200.0;
+
+            Console.WriteLine($"{usdAmount} USD = {converter.ConvertFromUSD(usdAmount):0.00} грн");
+            Console.WriteLine($"{eurAmount} EUR = {converter.ConvertFromEUR(eurAmount):0.00} грн");
+            Console.WriteLine($"{plnAmount} PLN = {converter.ConvertFromPLN(plnAmount):0.00} грн");
+        }
+    }
+}
